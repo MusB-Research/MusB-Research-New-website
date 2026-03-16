8a4ea4d15d9ba70400e8c2bfbb88331e5b8671a5
barenya123 <barenyamishra123@gmail.com>
2026-03-16 18:01:19 +0530

    Fix STATIC_ROOT configuration for Render deployment

--- backend/musb_backend/settings.py
+++ backend/musb_backend/settings.py
@@ -146,8 +146,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Media files (User uploads)
 MEDIA_URL = '/media/'
